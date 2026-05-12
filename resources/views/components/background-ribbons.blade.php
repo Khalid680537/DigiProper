6f967d23d3a47b34f020b6436a9e56bbff2bc3c2
Gyan <gyan@abctransport.co.in>
2026-05-13 01:41:58 +0530

    style: reposition background tint blobs to bottom-left and top-right corners

--- resources/views/components/background-ribbons.blade.php
+++ resources/views/components/background-ribbons.blade.php
@@ -1,11 +1,11 @@
 {{-- Subtle background pattern: dot grid with purple/orange tinted corners. --}}
 {{-- Fixed to the viewport, sits behind page content. --}}
 <div aria-hidden="true" class="pointer-events-none fixed inset-0 z-0 overflow-hidden">
-    {{-- Purple tint blob, top-left --}}
-    <div class="absolute -top-40 -left-40 h-[44rem] w-[44rem] rounded-full bg-primary-500/25 blur-3xl"></div>
+    {{-- Purple tint blob, bottom-left --}}
+    <div class="absolute -bottom-40 -left-40 h-[44rem] w-[44rem] rounded-full bg-primary-500/25 blur-3xl"></div>
 
-    {{-- Orange tint blob, bottom-right --}}
-    <div class="absolute -bottom-40 -right-40 h-[42rem] w-[42rem] rounded-full bg-accent-400/22 blur-3xl"></div>
+    {{-- Orange tint blob, top-right --}}
+    <div class="absolute -top-40 -right-40 h-[42rem] w-[42rem] rounded-full bg-accent-400/22 blur-3xl"></div>
 
     {{-- Dot grid overlay --}}
     <div class="absolute inset-0"
