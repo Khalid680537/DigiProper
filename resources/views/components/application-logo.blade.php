9bb698fe67def1f781d45c2259b01231948d5576
Gyan <gyan@abctransport.co.in>
2026-05-13 01:04:37 +0530

    feat: update accent color palette and refine brand identity in logo and sidebar typography

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,3 +1,3 @@
 <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="currentColor" {{ $attributes }}>
-    <path fill-rule="evenodd" clip-rule="evenodd" d="M12 2.25 21 11.25V20.5a1.25 1.25 0 0 1-1.25 1.25H4.25a1.25 1.25 0 0 1-1.25-1.25V11.25L12 2.25ZM11.7 17V13.4H11.4A.6.6 0 0 0 12.6 13.4H12.3V17Z"/>
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M12 2.25 21 11.25V20.5a1.25 1.25 0 0 1-1.25 1.25H4.25a1.25 1.25 0 0 1-1.25-1.25V11.25L12 2.25ZM9.5 14a2.5 2.5 0 0 1 5 0v7.75h-5z"/>
 </svg>
