2af031c2d50d9f6e7c524dd2000653c49a79e92c
Gyan <gyan@abctransport.co.in>
2026-05-14 12:21:19 +0530

    Update PWA on site

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,9 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+if ('serviceWorker' in navigator && import.meta.env.PROD) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js', { scope: '/' }).catch(() => {});
+    });
+}
